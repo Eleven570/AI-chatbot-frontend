0659c7a14570bb772ace54cdde7f305f679bf8e8
Eleven570 <99649681+Eleven570@users.noreply.github.com>
2025-06-22 17:07:21 +0900

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 import './index.css'
-import './custom.css'  
+import './custom.css'
+
 import App from './App.jsx'
+import NotionStyleEditor from './pages/NotionStyleEditor.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/detail/:id" element={<NotionStyleEditor />} /> 
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
